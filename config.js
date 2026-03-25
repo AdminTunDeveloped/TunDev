02e6a10b5fc73596a9d114ad99deed41203caa0b
AdminTunDeveloped <tunmediasocialmediaservices@gmail.com>
2026-03-25 22:51:42 +0700

    Add configuration for TUN Game Optimizer

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    softwareName: "TUN Game Optimizer",
+    version: "v2.1.3",
+    updateDate: "25/03/2026",
+    downloadAndroid: "#", // Thay bằng link tải thật sau
+    downloadIOS: "#",
+    githubRaw: "https://raw.githubusercontent.com/YOUR_USERNAME/TUN-DEVELOPED-SOFTWARE/main/data/"
+};
